149e93c76d71dcf4b1398e84ec496c7641e2a6b9
AbhishekMonarchEmmanual <abhimonarch60@gmail.com>
2023-06-14 02:55:43 +0530

    SECOND COMMIT WHERE I AM ADDING TWO MORE FUNCTIONS

--- real_class_object.py
+++ real_class_object.py
@@ -1,7 +1,8 @@
 import streamlit as st 
 import pandas as pd 
 import numpy as np 
-import seaborn as sns 
+import seaborn as sns
+import scipy as sp 
 
 class data_profile_streamlit:
     
@@ -34,7 +35,11 @@ class data_profile_streamlit:
                 self.overall_stats(data=self.data)
                 self.correlation(data=self.data)
                 self.normal(data=self.data)
+                self.box(data = self.data)
+                self.count(data = self.data)
+                self.reg(data=self.data)
                 self.groupby(data = self.data)
+                
             if ext[-1] == "xlsx":
                 self.data= pd.read_excel(upload)
                 self.show_dataset(data= self.data)
@@ -45,7 +50,11 @@ class data_profile_streamlit:
                 self.overall_stats(data=self.data)
                 self.correlation(data=self.data)
                 self.normal(data=self.data)
+                self.box(data = self.data)
+                self.count(data = self.data)
+                self.reg(data=self.data)
                 self.groupby(data = self.data)
+                
             else : 
                 st.write("PLEASE UPLAOD CORRECT CSV OR XLSX FORMAT")
                        
@@ -135,7 +144,7 @@ class data_profile_streamlit:
             st.pyplot()
             st.write(data.corr(method = 'pearson',numeric_only=True))
         if check == "NO":
-            st.text("You selected not check the correlation")
+            st.text("You selected not to check the correlation")
 # 8.GET TO CHECK IF THE DATA SET IS NORMALLY DISTRIBUTE OR NOT THROUGH DISPLOT
 
     def normal(self, data):
@@ -146,7 +155,55 @@ class data_profile_streamlit:
                 st.set_option('deprecation.showPyplotGlobalUse', False)
                 sns.distplot(new[i])
                 st.pyplot()
-# PERFORM GROUP BY    
+                
+# 9. GET TO SEE IF THE COUNT OF CATEGORICAL DATA SETS IN YOUR DATA SETS 
+    def count(self, data):
+        if st.checkbox("DO YOU WANT TO PLOT THE COUNT PLOTS???"):
+            value =  st.selectbox("Would like to see the count_plot of the columns??", (list(data.columns)), key = "count")
+            st.write(f"YOU WANT TO SEE THE COUNT_PLOT OF THE FOLLOWING COLUMN {value}") 
+            st.set_option('deprecation.showPyplotGlobalUse', False)
+            cx= sns.countplot(x = value, data= data)
+            for bars in cx.containers:
+                    cx.bar_label(bars)
+            st.pyplot()
+        
+# 10. Get Box PLot plot of your data 
+    def box(self, data):
+        if st.checkbox("DO YOU WANT TO PLOT BOX PLOT???", key = "BOX_PLOTING"):
+            box_val =  st.selectbox("Would like to see the box_plot of the columns??", (list(data.columns)), key = "box_plot")
+            st.write(f"YOU WANT TO SEE THE BOX_PLOT OF THE FOLLOWING COLUMN {box_val}") 
+            try:    
+                st.set_option('deprecation.showPyplotGlobalUse', False)
+                bx = sns.boxplot(x = box_val, data= data)
+                for boxes in bx.containers:
+                        bx.bar_label(boxes)
+                st.pyplot()
+            except Exception as e:
+                st.write(e)
+            
+# Check for the reg-plot for regression between individual attrbutes of datasets
+    def reg(self, data):
+        if st.checkbox("DO YOU WANT REG PLOT???", key = "REG_PLOTING"):
+            X_VAL =  st.selectbox("Would like to see the REG_PLOT of the columns-CHOOSE X COLUMN VALUE??", (list(data.columns)), key = "reg1_plot")
+            Y_VAL =  st.selectbox("Would like to see the REG_PLOT of the columns-CHOOSE Y COLUMN VALUE??", (list(data.columns)), key = "reg2_plot")
+            st.write(f"YOU WANT TO SEE THE REG_PLOT {X_VAL} vs {Y_VAL} ON THE FOLLOWING COLUMN ") 
+            
+            try :
+                st.set_option('deprecation.showPyplotGlobalUse', False)
+                
+                cx= sns.jointplot(x = X_VAL, y = Y_VAL, data= data, kind = 'reg')
+                regline= cx.ax_joint.get_lines()[0]
+                regline.set_color("red")
+                st.pyplot(cx)
+
+                st.write(data.corr(method = 'pearson',numeric_only=True))
+                
+            except Exception as e:
+                    print(e)
+
+
+                
+# 11. PERFORM GROUP BY    
     def groupby(self, data):    
              
         button1 = st.checkbox("Click Here if you want to perform the group by option")
